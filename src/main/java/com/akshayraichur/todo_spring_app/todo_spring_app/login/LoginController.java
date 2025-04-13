fbea45b304593846a3baec3f47450f14ffab14e1
Akshay Raichur <akshay.raichur9@gmail.com>
2025-04-13 20:26:57 +0530

    chore: understand loggin in spring

--- src/main/java/com/akshayraichur/todo_spring_app/todo_spring_app/login/LoginController.java
+++ src/main/java/com/akshayraichur/todo_spring_app/todo_spring_app/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.akshayraichur.todo_spring_app.todo_spring_app.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,16 +9,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private Logger logger = LoggerFactory.getLogger(getClass());
     /**
      * Whenever we want to pass anything from controller to view, you need to make use of Model.
      * Import ModelMap and give it a name. Then we can add anything to model and that will be available to jsp
      */
     @RequestMapping("/login")
     public String login(@RequestParam String redirectTo, ModelMap model){
-
         model.put("redirectTo", redirectTo);
+        logger.debug("Request param is {}", redirectTo);
+//        logger.info("I want this to be printed at info level");
+//        logger.warn("I want this to be printed at warn level");
 
-        System.out.println(redirectTo);
         return "login";
     }
 }
